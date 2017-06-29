fc2f58efdfaa84881e92eae448e44420e20741d5
KadoBOT <ricardo.ambrogi@gmail.com>
2017-06-29 15:49:15 +0200

    with react router

---
+++ src/Text.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = <span>¯\_(ツ)_/¯</span>;
+
+export { Test };
+
+export default Test;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 import App from './App';
+import Bundle from './routes/Bundle';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Example = props => <Bundle load={() => import('./modules/Example')} {...props} />;
+
+const Main = () => (
+  <Router>
+    <div>
+      <Link to="/">Home</Link>
+      {' '}
+      <Link to="/example">Example</Link>
+      <hr />
+      <Route exact path="/" component={App} />
+      <Route path="/example" component={Example} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
